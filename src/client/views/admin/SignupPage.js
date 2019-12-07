edea7f1a3e2d39a61271a2f695e3b66d098cb1cd
Derek Chan <dchan3@hawaii.edu>
2019-12-06 20:31:44 -1000

    Change login and signup links

--- src/client/views/admin/SignupPage.js
+++ src/client/views/admin/SignupPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { GeneratedForm, StyledDiv } from '../../reusables';
+import { GeneratedForm, StyledDiv, SamePageAnchor } from '../../reusables';
 
 export default () => [<GeneratedForm title="Sign Up" params={{
   username: { type: 'text', maximum: 20, required: true },
   email: { type: 'email', required: true },
   password: { type: 'password', required: true }
 }} formAction='users/register' redirectUrl='/admin' />,
-<StyledDiv><p>Have an account? Sign in <a href="/login">here</a>.</p>
+<StyledDiv><p>Have an account? Sign in <SamePageAnchor href="/login">
+  here</SamePageAnchor>.</p>
 </StyledDiv>];
