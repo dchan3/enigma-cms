6a78624b5086a26749cd210b47c3b634048f08a2
Derek Chan <dchan3@hawaii.edu>
2020-05-19 17:35:23 -1000

    Fix render markup typo

--- src/server/utils/render_markup.js
+++ src/server/utils/render_markup.js
@@ -5,10 +5,10 @@ import path from 'path';
 let renderMarkup = async function (templateBody, stuff) {
   let shortcodes = {}, code = '';
   if (fs.exists(path.join(process.env.DIRECTORY || __dirname, 'site-files/shortcodes.js'))) {
-    fs.readFileSync(path.join(process.env.DIRECTORY || __dirname, 'site-files/shortcodes.js'), 'utf8');
+    code = fs.readFileSync(path.join(process.env.DIRECTORY || __dirname, 'site-files/shortcodes.js'), 'utf8');
     shortcodes = eval(`(function() { return ${code}; })()`);
   }
-  
+
   for (let key in shortcodes) {
     TemplateParser.registerHelper(key, shortcodes[key]);
   }
