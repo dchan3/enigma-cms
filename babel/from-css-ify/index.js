34aaa6982aa4104baee253289da372015c719be0
Derek Chan <dchan3@hawaii.edu>
2020-06-07 15:18:53 -1000

    Add more From CSS extraction logic

--- babel/from-css-ify/index.js
+++ babel/from-css-ify/index.js
@@ -15,52 +15,60 @@ const first = '一', last = '龥',
   }, hashifyName = (name) => generateAlphabeticName(murmur(name));
 
 module.exports = function () {
-  let styles = {}, fn = '', getCssFromTemplateIfTernary = function(node, str = '') {
-    let retval = { consequent: str, alternate: str };
-    let attr = node.quasis[0].value.raw,
-      {
-        consequent: { value: ifTrue },
-        alternate: { value: ifFalse }
-      } = node.expressions[0];
-    retval.consequent +=  `${attr}${ifTrue}`;
-    retval.alternate += `${attr}${ifFalse}`;
-    return retval;
-  }, getCssFromBinaryExp = function({ left, right }, str = '') {
-    if (left.type === 'StringLiteral' && right.type === 'TemplateLiteral' && right.expressions[0].type === 'ConditionalExpression') {
-      return getCssFromTemplateIfTernary(right, left.value);
-    }
-    else if (left.type === 'StringLiteral' && right.type === 'BinaryExpression') {
-      return getCssFromBinaryExp(right, str + left.value);
-    }
-    else if (left.type === 'BinaryExpression' && right.type === 'StringLiteral') {
-      return getCssFromBinaryExp(left, str + right.value);
-    }
-    else if (left.type === 'BinaryExpression' && right.type === 'TemplateLiteral') {
-      let c = getCssFromBinaryExp(left), d = getCssFromTemplateIfTernary(right);
-      if (Object.keys(c).length === Object.keys(d).length) {
-        let e = {}
-        for (var k in c) {
-          e[k] = c[k] + (c[k].endsWith(";") ? '' : ';') + d[k];
+  let styles = {}, fn = '',
+    getCssFromTemplateIfTernary = function(node, str = '') {
+      let retval = { consequent: str, alternate: str };
+      let attr = node.quasis[0].value.raw,
+        {
+          consequent: { value: ifTrue },
+          alternate: { value: ifFalse }
+        } = node.expressions[0];
+      retval.consequent +=  `${attr}${ifTrue}`;
+      retval.alternate += `${attr}${ifFalse}`;
+      return retval;
+    }, getCssFromBinaryExp = function({ left, right }, str = '') {
+      if (left.type === 'StringLiteral' && right.type === 'TemplateLiteral' &&
+        right.expressions[0].type === 'ConditionalExpression') {
+        return getCssFromTemplateIfTernary(right, left.value);
+      }
+      else if (left.type === 'StringLiteral' &&
+        right.type === 'BinaryExpression') {
+        return getCssFromBinaryExp(right, str + left.value);
+      }
+      else if (left.type === 'BinaryExpression' &&
+        right.type === 'StringLiteral') {
+        return getCssFromBinaryExp(left, str + right.value);
+      }
+      else if (left.type === 'BinaryExpression' &&
+        right.type === 'TemplateLiteral') {
+        let c = getCssFromBinaryExp(left),
+          d = getCssFromTemplateIfTernary(right);
+        if (Object.keys(c).length === Object.keys(d).length) {
+          let e = {}
+          for (var k in c) {
+            e[k] = c[k] + (c[k].endsWith(";") ? '' : ';') + d[k];
+          }
+
+          return e;
         }
+      }
+      else if (left.type === 'TemplateLiteral' &&
+        right.type === 'TemplateLiteral') {
+        let lft = getCssFromTemplateIfTernary(left),
+          rgt = getCssFromTemplateIfTernary(right);
 
-        return e;
+        return {
+          "tt": str + lft.consequent + right.consequent,
+          "tf": str + lft.consequent + right.alternate,
+          "ft": str + lft.alternate + right.consequent,
+          "ff": str + lft.alternate + right.alternate
+        }
       }
     }
-    else if (left.type === 'TemplateLiteral' && right.type === 'TemplateLiteral') {
-      let lft = getCssFromTemplateIfTernary(left), rgt = getCssFromTemplateIfTernary(right);
-
-      return {
-        "tt": str + lft.consequent + right.consequent,
-        "tf": str + lft.consequent + right.alternate,
-        "ft": str + lft.alternate + right.consequent,
-        "ff": str + lft.alternate + right.alternate
-      }
-    }
-  }
 
   return {
     visitor: {
-      CallExpression({ node: { callee, arguments }}, { opts }) {
+      CallExpression({ node: { callee, arguments: args } }, { opts }) {
         if (fn === '') {
           if (opts && opts.toFile) fn = opts.toFile;
         }
@@ -69,30 +77,54 @@ module.exports = function () {
               type: styleType,
               value: styleValue,
               params: styleParams,
-              body: styleBody
-            }] = arguments, css = '';
+              body: styleBody,
+              properties: styleProperties
+            }] = args, css = '';
           if (styleType === 'StringLiteral') {
             css = styleValue;
           }
           else if (styleType === 'ArrowFunctionExpression') {
             if (styleParams[0].type === 'ObjectPattern') {
-              let paramList = styleParams[0].properties.map(({ value: { name }}) => name);
-              if (styleBody.type === 'BinaryExpression' && styleBody.operator == '+') {
-
+              let paramList = styleParams[0].properties.map(({
+                value: { name } }) => name);
+              if (styleBody.type === 'BinaryExpression' &&
+                styleBody.operator == '+') {
                 css = getCssFromBinaryExp(styleBody);
               }
             }
           }
+          else if (styleType === 'ObjectExpression') {
+            css = {};
+            for (var i = 0; i < styleProperties.length; i++) {
+              css[styleProperties[i].key.value] =
+                styleProperties[i].value.value;
+            }
+          }
+
           if (typeof css === 'string') {
             if (!css.endsWith(";")) css += ';';
             className = `fc-${hashifyName(`fc ${css}`)}`;
             styles[`.${className}`] = css;
           }
           else {
-            for (let i in css) {
-              let ac = css[i].endsWith(";") ? css[i] : (css[i] + ';')
-              className = `fc-${hashifyName(`fc ${ac}`)}`;
-              styles[`.${className}`] = ac;
+            if (Object.keys(css).includes('&')) {
+              // First aggregate all selectors
+              let mash = '';
+              for (let k in css) {
+                let ac = css[k].endsWith(";") ? css[k] : (css[k] + ';')
+                mash += k.replace(/&/g, '.fc') + '{' + css[k] + '}';
+              }
+              className = `fc-${hashifyName(`fc ${mash}`)}`;
+              for (let o in css) {
+                styles[o.replace(/&/g, `.${className}`)] = css[o];
+              }
+            }
+            else {
+              for (let i in css) {
+                let ac = css[i].endsWith(";") ? css[i] : (css[i] + ';');
+                className = `fc-${hashifyName(`fc ${ac}`)}`;
+                styles[`.${className}`] = ac;
+              }
             }
           }
         }
