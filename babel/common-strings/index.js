01d5736d13ebf2670e09eb809c4ab75ba439ffe4
Derek Chan <dchan3@hawaii.edu>
2021-06-28 20:41:28 -1000

    extract function

--- babel/common-strings/index.js
+++ babel/common-strings/index.js
@@ -1,5 +1,12 @@
 let t = require('@babel/types'), kwTable = require('./kw-table');
 
+function getValue(str, options, cb) {
+  let repl = (options.table && options.table[str]) || kwTable[str];
+  if (repl) {
+    cb(repl);
+  }
+};
+
 module.exports =
   function () {
     return {
@@ -7,12 +14,9 @@ module.exports =
         ObjectProperty(path, { opts }) {
           if (path.node.computed) { // when obj[computedValue]
             if (path.node.key.type === 'StringLiteral') { // obj["variable"]
-              if (opts.table && opts.table[path.node.key.value]) {
-                path.replaceWith(t.objectProperty(t.identifier(opts.table[path.node.key.value]), path.node.value, true));
-              }
-              if (kwTable[path.node.key.value] && typeof kwTable[path.node.key.value] === 'string') {
-                path.replaceWith(t.objectProperty(t.identifier(kwTable[path.node.key.value]), path.node.value, true));
-              }
+              getValue(path.node.key.value, opts, repl => {
+                path.replaceWith(t.objectProperty(t.identifier(repl), path.node.value, true));
+              });
             }
           }
         },
@@ -20,47 +24,37 @@ module.exports =
           if (path.node.value === 'history' && ['ImportDeclaration', 'CallExpression'].includes(path.parent.type)) {
             return;
           }
-          if (opts.table && opts.table[path.node.value] && !['VariableDeclarator', 'JSXAttribute'].includes(path.parent.type)) {
-            path.replaceWith(t.identifier(opts.table[path.node.value]));
-          }
-          if (kwTable[path.node.value] && !['VariableDeclarator', 'JSXAttribute'].includes(path.parent.type)) {
-            path.replaceWith(t.identifier(kwTable[path.node.value]));
-          }
+
+          getValue(path.node.value, opts, (repl) => {
+            if (!['VariableDeclarator', 'JSXAttribute'].includes(path.parent.type)) {
+              path.replaceWith(t.identifier(repl));
+            }
+          });
         },
         MemberExpression(path, { opts }) {
           if (path.node.computed) {
             if (path.node.property.type === 'StringLiteral' && path.node.property.value.length > 1) {
-              if (opts && opts.table && opts.table[path.node.property.value] && typeof opts.table[path.node.property.value] === 'string') {
-                path.replaceWith(t.memberExpression(
-                  path.node.object,
-                  t.identifier(opts.table[path.node.property.value]),
-                  true
-                ));
-              }
-              if (kwTable[path.node.property.value] && typeof kwTable[path.node.property.value] === 'string') {
-                path.replaceWith(t.memberExpression(
-                  path.node.object,
-                  t.identifier(kwTable[path.node.property.value]),
-                  true
-                ));
-              }
+              getValue(path.node.property.value, opts, repl => {
+                if (typeof repl === 'string') {
+                  path.replaceWith(t.memberExpression(
+                    path.node.object,
+                    t.identifier(repl),
+                    true
+                  ));
+                }
+              });
             }
           }
           else {
-            if (opts && opts.table && opts.table[path.node.property.name] && typeof opts.table[path.node.property.name] === 'string') {
-              path.replaceWith(t.memberExpression(
-                path.node.object,
-                t.identifier(opts.table[path.node.property.name]),
-                true
-              ));
-            }
-            if (kwTable[path.node.property.name] && typeof kwTable[path.node.property.name] === 'string') {
-              path.replaceWith(t.memberExpression(
-                path.node.object,
-                t.identifier(kwTable[path.node.property.name]),
-                true
-              ));
-            }
+            getValue(path.node.property.name, opts, (repl) => {
+              if (typeof repl === 'string') {
+                path.replaceWith(t.memberExpression(
+                  path.node.object,
+                  t.identifier(repl),
+                  true
+                ));
+              }
+            });
           }
         },
         Identifier(path) {
