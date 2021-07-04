5d9ab0daa4734c78c14a861d44fa709f3145bb29
Derek Chan <dchan3@hawaii.edu>
2021-07-04 12:17:41 -1000

    Persistent login thing

--- src/client/utils/api_request_async.js
+++ src/client/utils/api_request_async.js
@@ -1,9 +1,8 @@
-export const getRequest = function(endpoint, callback, config = { credentials: 'include' }) {
-  fetch(`/api/${endpoint}`, { ...config }).then(response => response.json()
+export const getRequest = function(endpoint, callback, config) {
+  fetch(`/api/${endpoint}`, { credentials: 'include', ...config }).then(response => response.json()
   ).then(callback);
 };
 
-export const postRequest = function (endpoint, body, callback,
-  config = { credentials: 'include' }) {
-  fetch(`/api/${endpoint}`, { method: 'post', body, ...config }).then(callback);
+export const postRequest = function (endpoint, body, callback, config) {
+  fetch(`/api/${endpoint}`, { method: 'post', body, credentials: 'include', ...config }).then(callback);
 }
